5ce4e865de585f1e7a8b5ed302e50f3e5f3d30ea
Vinod Tyagi <55526636+vinodtyagi@users.noreply.github.com>
2019-10-21 09:31:05 +0530

    Add files via upload

---
+++ testmaven/src/test/java/testmaven/Test1.java
@@ -0,0 +1,19 @@
+package testmaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
